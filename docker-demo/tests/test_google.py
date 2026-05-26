0c2ea4303ae8d72c31ddbec9e6addd86a3344616
Asena Karakaş <85061777+ASENA3276@users.noreply.github.com>
2026-05-26 16:21:38 +0300

    Add files via upload

---
+++ docker-demo/tests/test_google.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+def test_google_title():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    service = Service("/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=options)
+
+    driver.get("https://www.google.com")
+    assert "Google" in driver.title
+    driver.quit()
